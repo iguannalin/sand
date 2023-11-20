e07b685c16b11ee554abdc0845a8c5295955bc2b
seasons986 <annalin12345@gmail.com>
2023-11-20 00:29:57 -0500

    try again

--- index.js
+++ index.js
@@ -22,81 +22,86 @@ window.addEventListener("load", () => {
   }
 
   const container = document.getElementById("container");
-  // const banner = document.getElementById("banner");
-  // const start = document.getElementById("start");
-  // if (hasTouchScreen) banner.style.display = "none";
-  // else start.style.display = "none";
+  const start = document.getElementById("start");
+  if (!hasTouchScreen) start.style.display = "none";
 
   let points = [];
   let orientation = {alpha:0,beta:0};
   let xInc = 0;
   let yInc = 1;
   let isMoving = false;
+  let offsetWidth = window.innerWidth - 10;
+  let offsetHeight = window.innerHeight - 25;
 
   function handleMovePoints() {
     let newPoints = [];
-    // isMoving = true;
+    isMoving = true;
     points.forEach((p, i) => {
-      if (p.offsetTop != window.innerHeight - 25) newPoints.push(p);
-      else {
-        p.style.top = p.offsetTop - Math.random()*(i%15) +"px";
-        p.style.left = p.offsetLeft + Math.random()*10 +"px";
+      if (!hasTouchScreen || !DeviceMotionEvent) { // desktop doesn't have sensor, so just move linearly
+        if (p.offsetTop != offsetHeight) newPoints.push(p);
+        else {
+          p.style.top = p.offsetTop - Math.random()*(i%15) +"px";
+          p.style.left = p.offsetLeft + Math.random()*10 +"px";
+        }
+        if (Math.random() > 0.8) return; // stagger movements so it's non-linear
+        p.style.top = p.offsetTop + yInc +"px";
+      } else {
+        if ((10 < p.offsetLeft <= offsetWidth) && (10 < p.offsetTop <= offsetHeight)) newPoints.push(p);
+        p.style.left = p.offsetLeft + xInc +"px";
+        p.style.top = p.offsetTop + yInc +"px";
       }
-      if (Math.random() > 0.8) return;
-      // if (p.offsetLeft != 0 && p.offsetTop != 0&& p.offsetRight != window.innerWidth && p.offsetBottom != window.innerHeight) newPoints.push(p);
-      // p.style.left = p.offsetLeft + xInc +"px";
-      p.style.top = p.offsetTop + yInc +"px";
     });
     // console.log(xInc, yInc);
     points = newPoints;
-    // isMoving = false;
+    isMoving = false;
   }
-  // all sensor orientation code below from -- https://sensor-js.xyz/demo.html
-  // function handleOrientation(event) {
-  //   // alpha = x, beta = y, gamma = z
-  //   if (Math.abs(event.alpha - orientation.alpha) < 10) {
-  //     // do nothing
-  //     xInc = 0;
-  //   } else if (event.alpha > orientation.alpha) {
-  //     xInc = -1;
-  //     orientation.alpha = event.alpha;
-  //   } else {
-  //     xInc = 1;
-  //     orientation.alpha = event.alpha;
-  //   }
-  //   if (Math.abs(event.beta - orientation.beta) < 10) {
-  //     // do nothing
-  //     yInc = xInc == 0 ? 1 : 0;
-  //   } else if (event.beta > orientation.beta) {
-  //     yInc = 1;
-  //     orientation.beta = event.beta;
-  //   } else {
-  //     yInc = -1;
-  //     orientation.beta = event.beta;
-  //   }
-  //   if (!isMoving) handleMovePoints();
-  // }
 
-  // let isRunning = false;
-  // start.onclick = function(e) {
-  //   e.preventDefault();
-  //   // Request permission for iOS 13+ devices
-  //   if (
-  //     DeviceMotionEvent &&
-  //     typeof DeviceMotionEvent.requestPermission === "function"
-  //   ) {
-  //     DeviceMotionEvent.requestPermission();
-  //   }
+  // all sensor orientation code below from -- https://sensor-js.xyz/demo.html
+  function handleOrientation(event) {
+    // alpha = x, beta = y, gamma = z
+    if (Math.abs(event.alpha - orientation.alpha) < 10) {
+      // do nothing
+      xInc = 0;
+    } else if (event.alpha > orientation.alpha) {
+      xInc = -1;
+      orientation.alpha = event.alpha;
+    } else {
+      xInc = 1;
+      orientation.alpha = event.alpha;
+    }
+    if (Math.abs(event.beta - orientation.beta) < 10) {
+      // do nothing
+      yInc = xInc == 0 ? 1 : 0;
+    } else if (event.beta > orientation.beta) {
+      yInc = 1;
+      orientation.beta = event.beta;
+    } else {
+      yInc = -1;
+      orientation.beta = event.beta;
+    }
+    if (!isMoving) handleMovePoints();
+  }
+
+  let isRunning = false;
+  start.onclick = function(e) {
+    e.preventDefault();
+    // Request permission for iOS 13+ devices
+    if (
+      DeviceMotionEvent &&
+      typeof DeviceMotionEvent.requestPermission === "function"
+    ) {
+      DeviceMotionEvent.requestPermission();
+    }
     
-  //   if (isRunning) {
-  //     window.removeEventListener("deviceorientation", handleOrientation);
-  //     isRunning = false;
-  //   } else {
-  //     window.addEventListener("deviceorientation", handleOrientation);
-  //     start.style.visibility = "hidden";
-  //     isRunning = true;
-  //   }
-  // };
+    if (isRunning) {
+      window.removeEventListener("deviceorientation", handleOrientation);
+      isRunning = false;
+    } else {
+      window.addEventListener("deviceorientation", handleOrientation);
+      start.style.visibility = "hidden";
+      isRunning = true;
+    }
+  };
 
   // if (hasTouchScreen) 
   setInterval(() => {
