5da7c4ffde77b26137b9d09f22fe8b060dfbc835
seasons986 <annalin12345@gmail.com>
2023-11-19 19:19:13 -0500

    try

--- index.js
+++ index.js
@@ -1,84 +1,90 @@
 window.addEventListener("load", () => {
-  function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+  // mobile device check code from -- https://stackoverflow.com/a/72502261
+  let hasTouchScreen = false;
+  if ("maxTouchPoints" in navigator) {
+      hasTouchScreen = navigator.maxTouchPoints > 0;
+  } else if ("msMaxTouchPoints" in navigator) {
+      hasTouchScreen = navigator.msMaxTouchPoints > 0;
+  } else {
+      const mQ = window.matchMedia && matchMedia("(pointer:coarse)");
+      if (mQ && mQ.media === "(pointer:coarse)") {
+          hasTouchScreen = !!mQ.matches;
+      } else if ('orientation' in window) {
+          hasTouchScreen = true; // deprecated, but good fallback
+      } else {
+          // Only as a last resort, fall back to user agent sniffing
+          const UA = navigator.userAgent;
+          hasTouchScreen = (
+              /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) ||
+              /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA)
+          );
+      }
   }
+
   const container = document.getElementById("container");
-  // setInterval(() => {
-  //   const span = document.createElement("span");
+  const banner = document.getElementById("banner");
+  const start = document.getElementById("start");
+  if (hasTouchScreen) banner.style.display = "none";
+  else start.style.display = "none";
 
-  //   container.appendChild(span);
-  // }, 1000);
-  // if ( location.protocol != "https:" ) {
-  //   location.href = "https:" + window.location.href.substring( window.location.protocol.length );
-  //   }
+  let points = [];
+  let orientation = {alpha:0,beta:0};
+  let xInc = 1;
+  let yInc = 1;
+  let isMoving = false;
+
+  function handleMovePoints() {
+    isMoving = true;
+    points.forEach((p) => {
+      p.style.left = p.offsetLeft + xInc +"px";
+      p.style.top = p.offsetTop + yInc +"px";
+    })
+  }
+  // all sensor orientation code below from -- https://sensor-js.xyz/demo.html
+  function handleOrientation(event) {
+    // alpha = x, beta = y, gamma = z
+    if (event.alpha > orientation.alpha) {
+      xInc = 1;
+    } else {
+      xInc = -1;
+    }
+    if (event.beta > orientation.beta) {
+      yInc = 1;
+    } else {
+      yInc = -1;
+    }
+    orientation.alpha = event.alpha;
+    orientation.beta = event.beta;
+    if (!isMoving) handleMovePoints();
+  }
+
+  let isRunning = false;
+  start.onclick = function(e) {
+    e.preventDefault();
+    // Request permission for iOS 13+ devices
+    if (
+      DeviceMotionEvent &&
+      typeof DeviceMotionEvent.requestPermission === "function"
+    ) {
+      DeviceMotionEvent.requestPermission();
+    }
     
+    if (isRunning) {
+      window.removeEventListener("deviceorientation", handleOrientation);
+      isRunning = false;
+    } else {
+      window.addEventListener("deviceorientation", handleOrientation);
+      start.style.visibility = "hidden";
+      isRunning = true;
+    }
+  };
 
-function handleOrientation(event) {
-  console.log("orientation ", event)
-  // updateFieldIfNotNull('Orientation_a', event.alpha);
-  // updateFieldIfNotNull('Orientation_b', event.beta);
-  // updateFieldIfNotNull('Orientation_g', event.gamma);
-  // incrementEventCount();
-}
-
-function incrementEventCount(){
-  let counterElement = document.getElementById("num-observed-events")
-  let eventCount = parseInt(counterElement.innerHTML)
-  counterElement.innerHTML = eventCount + 1;
-}
-
-function updateFieldIfNotNull(fieldName, value, precision=10){
-  if (value != null)
-    document.getElementById(fieldName).innerHTML = value.toFixed(precision);
-}
-
-function handleMotion(event) {
-  console.log("motion ", event)
-  // updateFieldIfNotNull('Accelerometer_gx', event.accelerationIncludingGravity.x);
-  // updateFieldIfNotNull('Accelerometer_gy', event.accelerationIncludingGravity.y);
-  // updateFieldIfNotNull('Accelerometer_gz', event.accelerationIncludingGravity.z);
-
-  // updateFieldIfNotNull('Accelerometer_x', event.acceleration.x);
-  // updateFieldIfNotNull('Accelerometer_y', event.acceleration.y);
-  // updateFieldIfNotNull('Accelerometer_z', event.acceleration.z);
-
-  // updateFieldIfNotNull('Accelerometer_i', event.interval, 2);
-
-  // updateFieldIfNotNull('Gyroscope_z', event.rotationRate.alpha);
-  // updateFieldIfNotNull('Gyroscope_x', event.rotationRate.beta);
-  // updateFieldIfNotNull('Gyroscope_y', event.rotationRate.gamma);
-}
-
-let is_running = false;
-let demo_button = document.getElementById("start_demo");
-demo_button.onclick = function(e) {
-  e.preventDefault();
-  
-  // Request permission for iOS 13+ devices
-  if (
-    DeviceMotionEvent &&
-    typeof DeviceMotionEvent.requestPermission === "function"
-  ) {
-    DeviceMotionEvent.requestPermission();
-  }
-  
-  if (is_running){
-    window.removeEventListener("devicemotion", handleMotion);
-    window.removeEventListener("deviceorientation", handleOrientation);
-    demo_button.innerHTML = "Start demo";
-    demo_button.classList.add('btn-success');
-    demo_button.classList.remove('btn-danger');
-    is_running = false;
-  }else{
-    window.addEventListener("devicemotion", handleMotion);
-    window.addEventListener("deviceorientation", handleOrientation);
-    document.getElementById("start_demo").innerHTML = "Stop demo";
-    demo_button.classList.remove('btn-success');
-    demo_button.classList.add('btn-danger');
-    is_running = true;
-  }
-};
-
+  setInterval(() => {
+    const span = document.createElement("span");
+    span.innerHTML = ".";
+    span.style.top = window.innerHeight/2+"px";
+    span.style.left = window.innerWidth/2+"px";
+    points.push(span);
+    container.appendChild(span);
+  }, 2000);
 });
